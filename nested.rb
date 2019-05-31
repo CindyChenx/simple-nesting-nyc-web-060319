38cee6e9add545741203ed0d8877a093f46d2717
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-31 04:24:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,6 +112,7 @@ def changing_alan
      
      
 end
+programmer_hashp[:alan_kay][:known_for]="GUI"
 
 def adding_to_dennis
 	programmer_hash = 
@@ -131,3 +132,5 @@ def adding_to_dennis
      }
 
 end
+
+programmer_hash[:dennis_ritchie]
